b61ef10a4716c23e430b08315ddff42a14445a0f
ThawinXD <bto2168@gmail.com>
2025-04-04 20:25:20 +0700

    add path bookings

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { configDotenv } from 'dotenv';
 import express, { Express } from 'express';
 import morgan from 'morgan';
 import hotels from 'routes/hotels';
+import bookings from 'routes/bookings';
 import connectDB from './config/db';
 
 configDotenv({ path: '.env' });
@@ -12,6 +13,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/hotels', hotels);
+app.use('/bookings', bookings)
 
 const HOST = process.env.HOST || 'http://localhost';
 const PORT = process.env.PORT || 5050;
