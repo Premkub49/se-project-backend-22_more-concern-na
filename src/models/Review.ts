73dba5451ae9d573c58cc3d2beaaa9f5abed2c98
ThawinXD <bto2168@gmail.com>
2025-04-04 14:11:16 +0700

    add review mobel

---
+++ src/models/Review.ts
@@ -0,0 +1,37 @@
+import mongoose, { ObjectId } from "mongoose";
+
+export interface IReview {
+   booking: ObjectId;
+   createdAt: Date;
+   rating: number;
+   parentReview?: ObjectId;
+   comment: string;
+}
+
+const ReviewSchema = new mongoose.Schema({
+   booking: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Booking',
+      required: true,
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now(),
+   },
+   rating: {
+      type: Number,
+      required: [true, 'Please add a rating'],
+      min: 1,
+      max: 5,
+   },
+   parentReview: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review',
+   },
+   comment: {
+      type: String,
+      required: [true, 'Please add a comment'],
+   },
+})
+
+export default mongoose.model("Review", ReviewSchema);
