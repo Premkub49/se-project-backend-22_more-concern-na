de036c0c864e37dd943b4561b7edef9ce4f44003
ThawinXD <bto2168@gmail.com>
2025-04-04 23:38:56 +0700

    fix import protect

--- src/routes/hotels.ts
+++ src/routes/hotels.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { addHotel, getHotels } from '../controllers/hotels';
-import { protect } from '../middleware/auth';
 
+const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router
   .route('/')
   .get(getHotels)
-  .post(protect as express.RequestHandler, addHotel);
+  .post(protect, addHotel);
 export default router;
