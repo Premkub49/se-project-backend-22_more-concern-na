41815b9d5170d7850b0a54aa2041424d4df6aaf2
ThawinXD <bto2168@gmail.com>
2025-04-04 14:11:01 +0700

    add report model

---
+++ src/models/Report.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+export interface IReport {
+   review: mongoose.Schema.Types.ObjectId;
+   reportDate: Date;
+   reportDescription: string;
+}
+
+const ReportSchema = new mongoose.Schema({
+   review: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+      required: true,
+   },
+   reportDate: {
+      type: Date,
+      default: Date.now(),
+   },
+   reportDescription: {
+      type: String,
+      required: [true, "Please add a report description"],
+   },
+});
+
+export default mongoose.model("Report", ReportSchema);
