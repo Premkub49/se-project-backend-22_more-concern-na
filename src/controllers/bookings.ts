e940b064c09e7ff21781b38b2bb2d5492b38b957
ThawinXD <bto2168@gmail.com>
2025-04-06 16:44:28 +0700

    add check valid day function

--- src/controllers/bookings.ts
+++ src/controllers/bookings.ts
@@ -1,6 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 
 import Booking from '../models/Booking';
+function checkDayValid(
+   start: string,
+   end: string,
+   res?: Response
+) {
+   const startDate = new Date(start);
+   const endDate = new Date(end);
+   if(endDate.getDate() - startDate.getDate() > 3 * 24 * 60 * 60 * 1000) {
+      if(res)
+         res.status(400).json({ success: false, msg: 'cannot booking exceed 3 days' });
+      return false;
+   }
+
+   return true;
+}
 
 export async function getBookings(
    req: Request,
