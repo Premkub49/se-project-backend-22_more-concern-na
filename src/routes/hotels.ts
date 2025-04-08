98bccb4c55684aea92264a28c321f34544da7b9b
ThawinXD <bto2168@gmail.com>
2025-04-08 09:42:50 +0700

    add path /hotels/:hotelId/bookings

--- src/routes/hotels.ts
+++ src/routes/hotels.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { addHotel, getHotels } from '../controllers/hotels';
 import { protect } from '../middleware/auth';
+import bookingRouter from './bookings';
 
 const router = express.Router();
+
+router.use('/:hotelId/bookings', bookingRouter);
 
 router.route('/').get(getHotels).post(protect, addHotel);
 export default router;
