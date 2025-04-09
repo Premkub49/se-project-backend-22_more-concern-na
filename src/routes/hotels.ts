1e87a5cf4437238bd1265231f6de6899a81e9fc2
Keerati Setthasuk <keerati.se123@gmail.com>
2025-04-09 13:14:12 +0700

    Refactor hotel route parameters for consistency and clarity

--- src/routes/hotels.ts
+++ src/routes/hotels.ts
@@ -2,15 +2,19 @@ import express from 'express';
 import { addHotel, checkAvailable, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotels';
 import { authorize, protect } from '../middleware/auth';
 import bookingRouter from './bookings';
+import { addRoom, deleteRoom, updateRoom } from '../controllers/rooms';
 
 const router = express.Router();
 
 router.use('/:hotelId/bookings', bookingRouter);
 
 router.route('/').get(getHotels).post(protect,authorize("admin"), addHotel);
-router.route('/:id')
+router.route('/:hotelId')
 .get(getHotel)
 .put(protect,authorize("admin","hotelManager"),updateHotel)
 .delete(protect,authorize("admin"),deleteHotel);
-router.route('/:id/available').get(protect,checkAvailable);
+router.route('/:hotelId/available').get(protect,checkAvailable);
+router.route('/:hotelId/rooms').post(protect,authorize("admin","hotelManager"),addRoom)
+router.route('/:hotelId/rooms/:roomId').put(protect,authorize("admin","hotelManager"),updateRoom)
+.delete(protect, authorize("admin", "hotelManager"), deleteRoom);
 export default router;
