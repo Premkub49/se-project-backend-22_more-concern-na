fa0689edc334415e9ceb910d30cde2bea96ab6a3
ThawinXD <bto2168@gmail.com>
2025-04-12 11:31:34 +0700

    create updateRespond

--- src/controllers/respond.ts
+++ src/controllers/respond.ts
@@ -52,3 +52,46 @@ export async function addRespond( req: Request, res: Response, next: NextFunctio
       res.status(500).json({ success: false, msg: "Server Error" });
    }
 }
+
+export async function updateRespond( req: Request, res: Response, next: NextFunction) {
+   try {
+      if(!req.user) {
+         res.status(401).json({ success: false, msg: "Not authorized to access this route" });
+         return;
+      }
+
+      const respond: any = await Review.findById(req.params.id);
+      if(!respond) {
+         res.status(404).json({ success: false, msg: "Respond not found" });
+         return;
+      }
+
+      const populateBooking = {
+         path: 'booking',
+         select: 'hotel'
+      }
+      const review: any = await Review.findById(respond.reply).populate(populateBooking);
+      if(!review) {
+         res.status(404).json({ success: false, msg: "Review not found" });
+         return;
+      }
+      if(!review.booking) {
+         res.status(404).json({ success: false, msg: "Booking not found" });
+         return;
+      }
+
+      if(req.user.role !== 'admin' && review.booking.hotel.toString() !== req.user.hotel.toString()) {
+         res.status(401).json({ success: false, msg: "Not authorized to access this route" });
+         return;
+      }
+
+      respond.title = req.body.title;
+      respond.text = req.body.text;
+
+      await respond.save();
+      res.status(200).json({ success: true });
+   } catch (error: any) {
+      console.error(error.stack);
+      res.status(500).json({ success: false, msg: "Server Error" });
+   }
+}
