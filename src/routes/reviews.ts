48ab4e39271567da627604129b4eebaec8b1506a
ThawinXD <bto2168@gmail.com>
2025-04-12 11:35:00 +0700

    add path respond
    /hotels/:hotelId/bookings/:bookingId/reviews/:reviewId/respond

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { authorize, protect } from '../middleware/auth';
 import { addReview, updateReview } from 'controllers/review';
+import routerRespond from './respond';
 
 const router = express.Router();
+
+router.use('/:reviewId/respond', protect, authorize('admin', 'hotelManager'), routerRespond);
 
 router.route('/').post(protect, addReview);
 
