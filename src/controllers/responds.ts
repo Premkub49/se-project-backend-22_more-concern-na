5cb6fa369403f64b36112d0933b03f3ccd201fd4
knpmayy <kanokponbinhem@gmail.com>
2025-04-18 23:39:06 +0700

    add delete reply

--- src/controllers/responds.ts
+++ src/controllers/responds.ts
@@ -59,7 +59,10 @@ export async function updateRespond( req: Request, res: Response, next: NextFunc
          res.status(401).json({ success: false, msg: "Not authorized to access this route" });
          return;
       }
-      
+      if(req.user.role !== "admin" && req.user.role !== "hotelManager") {
+         res.status(401).json({ success: false, msg: "Not authorized to access this route" });
+         return;
+      }
       const populateBooking = {
          path: 'booking',
          select: 'hotel'
@@ -90,10 +93,50 @@ export async function updateRespond( req: Request, res: Response, next: NextFunc
       }
 
       await Review.updateOne({_id: reviewId},{$set: {reply: respond}});
-      res.status(201).json({ success: true });
+      res.status(200).json({ success: true });
    } catch (err: any) {
       console.error(err.stack);
-      //res.status(500).json({ success: false, msg: "Server Error" });
+      responseErrorMsg(res,500,err,'Server error');
+   }
+}
+
+export async function deleteRespond( req: Request, res: Response, next: NextFunction) {
+   try {
+      if(!req.user) {
+         res.status(401).json({ success: false, msg: "Not authorized to access this route" });
+         return;
+      }
+      if(req.user.role !== "admin" && req.user.role !== "hotelManager") {
+         res.status(401).json({ success: false, msg: "Not authorized to access this route" });
+         return;
+      }
+      const populateBooking = { 
+         path: 'booking',
+         select: 'hotel'
+      }
+      const review: any = await Review.findById(req.params.reviewId).populate(populateBooking);
+      if(!review) {
+         res.status(404).json({ success: false, msg: "Review not found" });
+         return;
+      }
+      if(!review.reply) {
+         res.status(400).json({ success: false, msg: "Respond not found" });
+         return;
+      }
+      if(!review.booking) {
+         res.status(404).json({ success: false, msg: "Booking not found" });
+         return;
+      }
+      if(req.user.role !== 'admin' && review.booking.hotel.toString() !== req.user.hotel?.toString()) {
+         res.status(403).json({ success: false, msg: "Not authorized to access this route" });
+         return;
+      }
+      const reviewId = new mongoose.Types.ObjectId(req.params.reviewId);
+      await Review.updateOne({_id: reviewId},{$set: {reply: null}});
+      res.status(200).json({ success: true});
+   }
+   catch (err: any) {
+      console.error(err.stack);
       responseErrorMsg(res,500,err,'Server error');
    }
 }
--- src/routes/respond.ts
+++ src/routes/respond.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import { authorize, protect } from '../middleware/auth';
-import { addRespond, updateRespond } from '../controllers/responds';
+import { addRespond, updateRespond, deleteRespond } from '../controllers/responds';
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').post(protect, addRespond).put(protect, updateRespond);
+router.route('/').post(protect, addRespond).put(protect, updateRespond).delete(protect, deleteRespond);
 
 export default router;
