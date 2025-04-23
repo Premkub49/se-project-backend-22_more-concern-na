796bba9ebcbfa96e25475461a40f3e474d389bf1
Muq1937 <muqkung@gmail.com>
2025-04-23 13:16:42 +0700

    add GetUsersPoints

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import {getUsers, updateRole, updateUser, updateUserPoint} from "../controllers/users";
+import {getUsers, getUsersPoints, updateRole, updateUser, updateUserPoint} from "../controllers/users";
 import { authorize, protect } from '../middleware/auth';
 const router = express.Router();
 router.route("/").get(protect,authorize("admin"),getUsers).put(protect,updateUser);
 router.route("/role").put(protect,authorize("admin"),updateRole);
 router.route("/points/:userId").put(protect,authorize("admin"),updateUserPoint);
+router.route("/points").get(protect,authorize("admin"),getUsersPoints);
 
 
 export default router;
