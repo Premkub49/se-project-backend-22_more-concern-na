333d04f29b10de1fa456e7fad04c082165c7ca1d
Keerati Setthasuk <keerati.se123@gmail.com>
2025-04-24 13:22:59 +0700

    Update responseErrorMsg to handle production environment

--- src/controllers/libs/responseMsg.ts
+++ src/controllers/libs/responseMsg.ts
@@ -1,6 +1,6 @@
 import {Response} from "express";
 export default function responseErrorMsg(res:Response,status:number, err: string, msg: string){
-    if(process.env.NODE_ENV === "development"){
+    if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production"){
         res.status(status).json({success: false, msg: err});
     }
     else {
